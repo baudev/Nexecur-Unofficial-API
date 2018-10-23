1e6abf28fdbaec98024f237f3b10d995932988a2
quelqun007 <quelqun007@gmail.com>
2018-10-23 20:41:56 +0200

    Adapt API to Promise

--- test/tests.ts
+++ test/tests.ts
@@ -1,32 +1,27 @@
 import {NexecurAPI} from "../Controllers/NexecurAPI";
-import {AlarmStatus} from "../Models/AlarmStatus";
 
 var assert = require('assert');
 
-describe('NexecurAPI', function() {
+describe('NexecurAPI', function () {
     describe('getAlarmStatus', function () {
 
-        it('should return 0 or 1', function (done) {
-            NexecurAPI.getAlarmStatus((response => {
-                if(response != (AlarmStatus.Disabled && AlarmStatus.Enabled)){
-                    done("error");
-                } else {
-                    done();
-                }
-            }))
+        it('should get a response', function (done) {
+            NexecurAPI.getAlarmStatus().then(() => {
+                done();
+            }).catch(() => {
+                done("error");
+            })
 
         });
     });
 
-    describe('getHistoric', function() {
-        it('should be an array', function(done) {
-            NexecurAPI.getHistoric((response => {
-               if(Array.isArray(response)){
-                   done();
-               } else {
-                   done("error");
-               }
-            }))
+    describe('getHistoric', function () {
+        it('should get a response', function (done) {
+            NexecurAPI.getHistoric().then((res) => {
+                done();
+            }).catch(() => {
+                done("error");
+            })
 
         });
     });
